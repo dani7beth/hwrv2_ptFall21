be442d31167a53daa36dfc07b760360b986a08f1
dani7beth <danielledona28@gmail.com>
2021-08-27 19:21:58 -0600

    finished

--- movies.rb
+++ movies.rb
@@ -1,89 +1,71 @@
+require_relative 'wallet.rb'
+# require_relative 'file.rb'
+
 @movies = [
-  {name: "Jaws", rating: "PG", genre: "thriller"},
-  {name: "Halloween", rating: "R", genre: "horror"}
+  {name: "Jaws", rating: "PG", genre: "thriller", ticket_price: 10.50},
+  {name: "Halloween", rating: "R", genre: "horror", ticket_price: 10.50},
+  {name: "Raiders of the Lost Ark", rating: "PG", genre: "action", ticket_price: 10.50},
+  {name: "Monty Python and the Holy Grail", rating: "PG", genre: "comedy", ticket_price: 10.50},
+  {name: "E.T. the Extra-Terrestrial", rating: "PG", genre: "adventure", ticket_price: 10.50}
 ]
+
 
 
 # @movies.each do |movie|
 #   puts movie[:name]
 # end
+
 def menu
-  puts "What would you like to do?"
-  puts "1). Create a movie"
-  puts "2). View all movies"
-  puts "3). Update a movie"
-  puts "4). Delete a movie"
-  puts "5). exit"
-end
-
-def add_movie
-  @movie = {name: "", rating: "", genre: ""}
-  puts "What is the name of your movie?"
-  @movie[:name] = gets.strip
-  # @movie[:name] = name
-  puts "What is the rating of the movie?"
-  @movie[:rating] = gets.strip
-  puts "What is the genre?"
-  @movie[:genre] = gets.strip
-
-  # @movies << @movie
-  @movies.push(@movie) 
+  puts "Welcome to the movie theater!"
+  puts "How much to you have to spend?"
+  money = gets.strip.to_f
+  @wallet = Wallet.new(money)
+  if @wallet.validate_money(money)
+    puts "Your current balance is: $#{@wallet.current_balance}"
+    puts
+    puts "What movie would you like to see?"
+    view_movies
+  else
+    puts "invalid amount. try again"
+    menu
+  end
 end
 
 def view_movies
   # #{} <- allows you to use a variable within a string
   @movies.each_with_index do |movie,index|
-    puts "#{index + 1}). #{movie[:name]} - #{movie[:rating]}"
-    puts movie[:genre]
+    puts "#{index + 1}). #{movie[:name]} - #{movie[:rating]} - #{movie[:genre]}"
+    puts "$#{movie[:ticket_price]}"
     puts
   end
 end
 
-def update_movie(index)
-  puts "What is the new name?"
-  @movies[index][:name] = gets.strip
-  puts "What is the new rating?"
-  @movies[index][:rating] = gets.strip
-  puts "What is the new genre?"
-  @movies[index][:genre] = gets.strip
-end
-
-def delete_movie(index)
-  # .delete_at(index)
-  @movies.delete_at(index)
-  puts "Movie deleted"
-  view_movies
+def handle_movie_select(index)
+  money = @wallet.current_balance - @movies[index][:ticket_price]
+  if @wallet.validate_money(money)
+    @wallet.subtract_money(@movies[index][:ticket_price])
+    puts "Your new balance is: $#{@wallet.current_balance}"
+    puts "Enjoy the movie!"
+  else
+    puts "sorry you cannot see this movie"
+  end
 end
 
 def app
   menu
   @option = gets.strip.to_i
 
-  # if @option == 1
-  #   #do something
-  # elsif @option == 2
-
   case @option
   when 1
-    add_movie
-    app
+    handle_movie_select(@option - 1)
   when 2
-    view_movies
-    app
+    handle_movie_select(@option - 1)
   when 3
-    puts "What movie do you want to update?"
-    view_movies
-    index = gets.strip.to_i
-    update_movie(index - 1)
-    app
+    handle_movie_select(@option - 1)
   when 4
-    puts "What movie do you want to delete?"
-    view_movies
-    index = gets.strip.to_i
-    delete_movie(index - 1)
-    app
+    handle_movie_select(@option - 1)
   when 5
-    exit
+    handle_movie_select(@option - 1)
   else
     puts "Please enter one of the number options"
     app
